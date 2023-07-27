7c11abc0be1ba9cd49da07ae8345f1a9951ba671
Dávid Deák <david.deak@zenitech.co.uk>
2023-07-27 22:27:57 +0200

    add config files for secrets

--- NetworkingLayer/NetworkingLayer/ApiConstants.swift
+++ NetworkingLayer/NetworkingLayer/ApiConstants.swift
@@ -1,6 +1,8 @@
+import Foundation
+
 public enum ApiConstants {
     static let host = "https://api.themoviedb.org/3"
 
     public static let apiKey = "api_key"
-    public static let apiKeyValue = "0b1a18e2b899d214aba36f03889b819e"
+    public static let apiKeyValue = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
 }
